650f3ba68350c0fa5fe64253bd6f7ef9ac0425ee
jamshoode <54182934+jamshoode@users.noreply.github.com>
2021-04-04 15:59:55 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.getElementById("burger").onclick = function() {
 	document.getElementById("burger").classList.toggle("change")
 	document.getElementById("burger__menu").classList.toggle("active")
 	document.getElementById("body").classList.toggle("workin")
+	window.scrollTo(0, 0)
 }
 
 document.getElementById("burgerCloseBtn").onclick = function() {
